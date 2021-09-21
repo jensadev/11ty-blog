88ba52d9757c023814f9f790e491d5928fbae5d7
Jens Andreasson <jens.andreasson@ga.ntig.se>
2021-09-21 11:19:20 +0200

    ✨ rss meta changes osv

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,12 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 module.exports = function (eleventyConfig) {
     // eleventyConfig.addPassthroughCopy("src/css");
     // eleventyConfig.addWatchTarget("src/css");
 
     eleventyConfig.addPlugin(syntaxHighlight);
+    eleventyConfig.addPlugin(pluginRss);
 
     eleventyConfig.addWatchTarget("src/scss");
 
--- src/_data/github.js
+++ src/_data/github.js
@@ -1,9 +1,14 @@
 const axios = require('axios');
 
 module.exports = async () => {
-    const { data } = await axios.get('https://api.github.com/users/jensti');
-
-    console.log(data);
-
-    return data;
+    axios
+        .get('https://api.github.com/users/jensnti')
+        .then(function (response) {
+            const { data } = response;
+            return data;
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        });
 };
